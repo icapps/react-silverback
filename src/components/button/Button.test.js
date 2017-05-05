61ee00deb70e89b046cfaa8431c856f77d21efc1
Jasper Vercammen <jasper.vercammen@icapps.com>
2017-05-05 08:50:00 +0200

    Added extra test on click event button

--- src/components/button/Button.test.js
+++ src/components/button/Button.test.js
@@ -2,23 +2,30 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Button from './Button';
 
-it('should render a button', () => {
-  const wrapper = shallow(
-    <Button onClickCallback={() => {}}>This is a Button</Button>
-  );
-  expect(wrapper).toMatchSnapshot();
-});
+describe('Button Component', () => {
+  test('Render it as a button', () => {
+    const mockFn = jest.fn();
+    const wrapper = shallow(
+      <Button onClickCallback={mockFn}>This is a Button</Button>
+    );
+    expect(wrapper).toMatchSnapshot();
 
-it('should render a \'a\' link', () => {
-  const wrapper = shallow(
-    <Button href='http://google.com'>This is a href link!</Button>
-  );
-  expect(wrapper).toMatchSnapshot();
-});
+    wrapper.find('button').simulate('click');
+    expect(mockFn).toHaveBeenCalled();
+  });
 
-it('should render a react router Link', () => {
-  const wrapper = shallow(
-    <Button link='/home'>This is a React Router Link!</Button>
-  );
-  expect(wrapper).toMatchSnapshot();
+
+  it('should render a \'a\' link', () => {
+    const wrapper = shallow(
+      <Button href='http://google.com'>This is a href link!</Button>
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render a react router Link', () => {
+    const wrapper = shallow(
+      <Button link='/home'>This is a React Router Link!</Button>
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
 });
