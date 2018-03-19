d051967157cf21f38853832760c564dbc121e39c
jellemannaerts <jelle.mannaerts@icapps.com>
2018-03-19 09:42:14 +0100

    fixing table test issue

--- src/components/table/Table.test.js
+++ src/components/table/Table.test.js
@@ -12,11 +12,11 @@ describe('Table Component', () => {
       <Table
         keys={['key', 'key1', 'key2', 'key3']}
         listItems={[
-          { id: 'id1', key: "test 1", key1: 1, key2: false, key3: new Date(1521448332271) },
-          { id: 'id2', key: "test 2", key1: 2, key2: true, key3: new Date(1521448332271) },
-          { id: 'id3', key: "test 3", key1: 3, key2: true, key3: new Date(1521448332271) },
+          { id: 'id1', key: "test 1", key1: 1, key2: false, key3: new Date('2018-03-19T08:32:12.271+00:00') },
+          { id: 'id2', key: "test 2", key1: 2, key2: true, key3: new Date('2018-03-19T08:32:12.271+00:00') },
+          { id: 'id3', key: "test 3", key1: 3, key2: true, key3: new Date('2018-03-19T08:32:12.271+00:00') },
           { id: 'id4', key: "test 4", key2: false },
-          { id: 'id5', key: "test 5", key1: 4, key2: true, key3: new Date(1521448332271) },
+          { id: 'id5', key: "test 5", key1: 4, key2: true, key3: new Date('2018-03-19T08:32:12.271+00:00') },
         ]}
         handleRowClick={() => { }}
         handleRemoveItem={() => { }}
@@ -31,11 +31,11 @@ describe('Table Component', () => {
         keys={['key', 'key1', 'key2', 'key3']}
         listItems={
           [
-            { id: 'id1', key: "test 1", key1: 1, key2: false, key3: new Date(1521448332271) },
-            { id: 'id2', key: "test 2", key1: 2, key2: true, key3: new Date(1521448332271) },
-            { id: 'id3', key: "test 3", key1: 3, key2: true, key3: new Date(1521448332271) },
+            { id: 'id1', key: "test 1", key1: 1, key2: false, key3: new Date('2018-03-19T08:32:12.271+00:00') },
+            { id: 'id2', key: "test 2", key1: 2, key2: true, key3: new Date('2018-03-19T08:32:12.271+00:00') },
+            { id: 'id3', key: "test 3", key1: 3, key2: true, key3: new Date('2018-03-19T08:32:12.271+00:00') },
             { id: 'id4', key: "test 4", key2: false },
-            { id: 'id5', key: "test 5", key1: 4, key2: true, key3: new Date(1521448332271) },
+            { id: 'id5', key: "test 5", key1: 4, key2: true, key3: new Date('2018-03-19T08:32:12.271+00:00') },
           ]}
         dateFormat="MM/DD/YYYY"
         handleRowClick={() => { }}
