d5cabad2e06941245e9fa5d4343f46f8574f7b90
jellemannaerts <jelle.mannaerts@icapps.com>
2018-03-19 08:49:16 +0100

    added Table component

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Navbar from './navbar/NavBar';
 import BasicInput from './input/BasicInput';
 import Checkbox from './input/Checkbox';
 import Dropdown from './input/Dropdown';
+import Table from './table/Table';
 
-export { Header, Navbar, BasicInput, Checkbox, Dropdown };
+export { Header, Navbar, BasicInput, Checkbox, Dropdown, Table };
