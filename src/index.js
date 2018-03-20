6a92bc54fe5f30875a47654eb2c2311f7735eb3d
jellemannaerts <jelle.mannaerts@icapps.com>
2018-03-20 12:29:37 +0100

    code cleanup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import 'babel-polyfill';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import createStore from './redux/createStore';
