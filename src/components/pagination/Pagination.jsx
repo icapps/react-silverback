cff60378276b12fb082a5c57f7da6433dfe2de08
jellemannaerts <jelle.mannaerts@icapps.com>
2018-03-23 10:06:29 +0100

    show less pages

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -4,6 +4,7 @@ import './pagination.css';
 import { Dropdown } from '../index';
 
 const pageLimitOptions = [5, 10, 25, 50, 100];
+const visiblePages = 5;
 
 class Pagination extends React.Component {
   constructor() {
@@ -11,29 +12,39 @@ class Pagination extends React.Component {
     this.state = {
       activePage: 0,
       pageLimit: 10,
+      minPage: 1,
+      maxPage: visiblePages,
     };
   }
 
-  handleClick = (pageNumber) => {
-    this.setState({ activePage: pageNumber });
-    this.props.handleClick(pageNumber, this.state.pageLimit);
+  changePage = (pageNumber, pageLimit = this.state.pageLimit) => {
+    let maxPage = 1;
+    do {
+      maxPage += (visiblePages - 1);
+    } while (pageNumber >= (maxPage - 1));
+    const minPage = maxPage - (visiblePages - 1);
+    this.setState({ activePage: pageNumber, minPage, maxPage });
+    this.props.handleClick(pageNumber, pageLimit);
   }
 
-  handlePageLimitChange = event => {
+  changePageLimit = event => {
     const pageLimit = parseInt(event.target.value, 10);
-    this.setState({ pageLimit, activePage: 0 });
-    this.props.handleClick(this.state.activePage, pageLimit);
+    this.setState({ pageLimit });
+    this.changePage(0, pageLimit);
   }
 
   renderPages = nrOfPages => {
     return Array(nrOfPages).fill().map((page, index) => {
-      return (
-        <li key={`pagination-${index}`} className={`page-item ${this.state.activePage === index ? 'active' : ''}`} onClick={() => this.handleClick(index)}>
-          <span className="page-link">
-            {this.state.activePage === index && <span className="sr-only">Current</span>}{index + 1}
-          </span>
-        </li>
-      );
+      if ((index + 1) >= this.state.minPage && (index + 1) <= this.state.maxPage) {
+        return (
+          <li key={`pagination-${index}`} className={`page-item ${this.state.activePage === index ? 'active' : ''}`} onClick={() => this.changePage(index)}>
+            <span className="page-link">
+              {this.state.activePage === index && <span className="sr-only">Current</span>}{index + 1}
+            </span>
+          </li>
+        );
+      }
+      return null;
     });
   }
 
@@ -45,18 +56,38 @@ class Pagination extends React.Component {
 
     return (
       <div className="pagination-container">
-        <div className="pagination-group">
+        <div className="pagination-group pages">
           <span className="pagination-label">Showing {startOfRange} to {endOfRange > props.totalCount ? props.totalCount : endOfRange} of {props.totalCount}</span>
           <nav aria-label="Pagination">
             <ul className="pagination">
-              <li className={`page-item ${state.activePage === 0 ? 'disabled' : ''}`} onClick={() => state.activePage !== 0 && this.handleClick(state.activePage - 1)}>
+              <li className={`page-item ${state.activePage === 0 ? 'disabled' : ''}`} onClick={() => state.activePage !== 0 && this.changePage(state.activePage - 1)}>
                 <span className="page-link" aria-label="Previous">
                   <span aria-hidden="true">&laquo;</span>
                   <span className="sr-only">Previous</span>
                 </span>
               </li>
+              {this.state.minPage > 1 && (
+                <React.Fragment>
+                  <li className="page-item" onClick={() => this.changePage(0)}>
+                    <span className="page-link">1</span>
+                  </li>
+                  <li className="page-item disabled ellipsis">
+                    <span className="page-link">...</span>
+                  </li>
+                </React.Fragment>
+              )}
               {this.renderPages(nrOfPages)}
-              <li className={`page-item ${state.activePage === nrOfPages - 1 ? 'disabled' : ''}`} onClick={() => state.activePage !== nrOfPages - 1 && this.handleClick(state.activePage + 1)}>
+              {this.state.maxPage < nrOfPages && (
+                <React.Fragment>
+                  <li className="page-item disabled ellipsis">
+                    <span className="page-link">...</span>
+                  </li>
+                  <li className="page-item" onClick={() => this.changePage(nrOfPages - 1)}>
+                    <span className="page-link">{nrOfPages}</span>
+                  </li>
+                </React.Fragment>
+              )}
+              <li className={`page-item ${state.activePage === nrOfPages - 1 ? 'disabled' : ''}`} onClick={() => state.activePage !== nrOfPages - 1 && this.changePage(state.activePage + 1)}>
                 <span className="page-link" aria-label="Next">
                   <span aria-hidden="true">&raquo;</span>
                   <span className="sr-only">Next</span>
@@ -66,7 +97,7 @@ class Pagination extends React.Component {
           </nav>
         </div>
         <div className="pagination-group">
-          <Dropdown id="pageLimit" value={state.pageLimit} handleChange={this.handlePageLimitChange} options={pageLimitOptions} isLabelShown={false} />
+          <Dropdown id="pageLimit" value={state.pageLimit} handleChange={this.changePageLimit} options={pageLimitOptions} isLabelShown={false} />
           <span className="page-limit-label">Items per page</span>
         </div>
       </div>
--- src/services/dataTypeService.js
+++ src/services/dataTypeService.js
@@ -1,7 +1,7 @@
 import { Network } from '../utils';
 
 export const get = async (page, limit, sortField, sortOrder) => {
-  const pagination = `?page=${page}&limit=${limit}`;
+  const pagination = `?offset=${page}&limit=${limit}`;
   const sort = (sortField && sortField !== '') ? `sortField=${sortField}&sortOrder=${sortOrder}` : '';
   const result = await Network.get(`/datatype${pagination}${sort}`);
   return result;
