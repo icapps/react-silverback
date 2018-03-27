d610406fd3c4f7979e7618f33b648a1f04db35c4
jellemannaerts <jelle.mannaerts@icapps.com>
2018-03-27 10:09:11 +0200

    Made a better id for the browser

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,6 +1,14 @@
 import { Network } from '../utils';
 
 export const login = async (email, password) => {
-  const result = await Network.post('/auth/login', { username: email, password, deviceId: `${window.navigator.userAgent}-${email}` });
+  const result = await Network.post('/auth/login', { username: email, password, deviceId: getDeviceId(email) });
   return result;
+};
+
+const getDeviceId = email => {
+  if (localStorage.getItem('DEVICE_ID')) {
+    return localStorage.getItem('DEVICE_ID');
+  }
+  localStorage.setItem('DEVICE_ID', `${Math.random()}${new Date().getTime()}${window.navigator.userAgent}${email}`.split(' ').join());
+  return localStorage.getItem('DEVICE_ID');
 };
