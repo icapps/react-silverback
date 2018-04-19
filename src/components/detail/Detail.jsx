a315555217c34c329fbf0e62ed6209340ec1795d
jellemannaerts <jelle.mannaerts@icapps.com>
2018-04-19 15:49:54 +0200

    SIAP-102: Edit user - fixed unhandled rejection

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -15,6 +15,7 @@ class Detail extends React.Component {
     let inputItemState = {};
     this.props.inputItems.forEach(item => inputItemState[item.id] = item.value);
     this.setState({ inputItemState });
+    return true;
   }
 
   handleChange = event => {
@@ -23,7 +24,7 @@ class Detail extends React.Component {
 
   save = async () => {
     const result = await this.props.update(this.props.id, this.state.inputItemState);
-    if (result.action.type.includes('FULFILLED')) {
+    if (result.action && result.action.type.includes('FULFILLED')) {
       this.setState({ isSaved: true });
     }
   }
