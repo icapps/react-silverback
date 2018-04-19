9feb9129b459b2152a998fa4f47ae61f3574bc71
jellemannaerts <jelle.mannaerts@icapps.com>
2018-04-19 15:31:15 +0200

    SIAP-7: updated backend call

--- src/services/versionControlService.js
+++ src/services/versionControlService.js
@@ -1,6 +1,6 @@
 import { Network } from '../utils';
 
 export const get = async () => {
-  const result = await Network.get('/version');
+  const result = await Network.get('/config/version');
   return result;
 };
