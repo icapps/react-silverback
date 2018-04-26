bea0210c3657e504c0d5bbc55cde9e64c7110992
jellemannaerts <jelle.mannaerts@icapps.com>
2018-04-26 08:45:29 +0200

    made the state flatter in the ForgotPassword component

--- src/scenes/forgotPassword/ForgotPassword.jsx
+++ src/scenes/forgotPassword/ForgotPassword.jsx
@@ -12,23 +12,22 @@ class ForgotPassword extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: { value: '', isValid: true, errorMessage: strings.LOGIN_EMAIL_REQUIRED },
+      email: '',
+      isValid: true,
+      errorMessage: strings.LOGIN_EMAIL_REQUIRED,
       isForgotPasswordSent: false,
     };
   }
 
-  handleChange = event => this.setState({ email: { ...this.state.email, value: event.target.value } });
+  handleChange = event => this.setState({ email: event.target.value });
 
   handleForgotPassword = async () => {
     await this.setState({
-      email: {
-        ...this.state.email,
-        isValid: this.state.email.value !== '' && regexes.EMAIL.test(this.state.email.value),
-        errorMessage: this.state.email.value !== '' ? strings.LOGIN_EMAIL_VALIDATION : strings.LOGIN_EMAIL_REQUIRED,
-      },
+      isValid: this.state.email !== '' && regexes.EMAIL.test(this.state.email),
+      errorMessage: this.state.email !== '' ? strings.LOGIN_EMAIL_VALIDATION : strings.LOGIN_EMAIL_REQUIRED,
     });
-    if (this.state.email.isValid) {
-      const result = await this.props.forgotPassword(this.state.email.value);
+    if (this.state.isValid) {
+      const result = await this.props.forgotPassword(this.state.email);
       if (result.action && result.action.type === constants.FORGOT_PASSWORD_FULFILLED) {
         this.setState({ isForgotPasswordSent: true });
       }
@@ -46,7 +45,7 @@ class ForgotPassword extends React.Component {
         {props.isError && <div className="alert alert-danger text-center" role="alert"> {props.errorMessage} </div>}
         <main className='forgot-password'>
           <h2>{strings.RESET_PASSWORD}</h2>
-          <p className="forgot-password-succes-text">{strings.formatString(strings.RESET_PASSWORD_TEXT, { email: state.email.value !== '' ? <span>state.email.value</span> : strings.YOU })}</p>
+          <p className="forgot-password-succes-text">{strings.formatString(strings.RESET_PASSWORD_TEXT, { email: state.email !== '' ? <span>state.email</span> : strings.YOU })}</p>
           <Button className="btn-primary" text={strings.GO_BACK_TO_LOGIN} handleClick={this.goBackToLogin} />
         </main>
       </div>
@@ -57,7 +56,7 @@ class ForgotPassword extends React.Component {
         <main className='forgot-password'>
           <h2>{strings.FORGOT_PASSWORD_QUESTION}</h2>
           <p>{strings.FORGOT_PASSWORD_TEXT}</p>
-          <BasicInput id="email" label={strings.EMAIL} value={state.email.value} handleChange={this.handleChange} isValid={state.email.isValid} errorMessage={state.email.errorMessage} />
+          <BasicInput id="email" label={strings.EMAIL} value={state.email} handleChange={this.handleChange} isValid={state.isValid} errorMessage={state.errorMessage} />
           <Button className="btn-primary" text={strings.RETRIEVE_PASSWORD} handleClick={this.handleForgotPassword} />
         </main>
       </div>
