e324bb6cd7dd4bb10873f809604fa6bba6dec0e2
jellemannaerts <jelle.mannaerts@icapps.com>
2018-05-15 08:53:55 +0200

    fix comments

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -4,9 +4,9 @@ import { Spinner } from '..';
 
 const Button = props => (
   <button type="button" className={`btn ${props.className}`} onClick={props.handleClick} disabled={props.isDisabled || props.isPending}>
-    {props.icon && <img src={props.icon} alt="+" />}
+    {props.leftIcon && <img src={props.leftIcon} alt="" />}
     {props.isPending ? <Spinner hasContainer={false} spinnerClassName={'button-spinner'} /> : <span>{props.text}</span>}
-    {props.rightIcon && <img src={props.rightIcon} alt="+" />}
+    {props.rightIcon && <img src={props.rightIcon} alt="" />}
   </button>
 );
 
@@ -14,7 +14,7 @@ Button.propTypes = {
   className: PropTypes.string,
   handleClick: PropTypes.func.isRequired,
   text: PropTypes.string.isRequired,
-  icon: PropTypes.string,
+  leftIcon: PropTypes.string,
   rightIcon: PropTypes.string,
   isDisabled: PropTypes.bool,
   isPending: PropTypes.bool,
@@ -22,7 +22,7 @@ Button.propTypes = {
 
 Button.defaultProps = {
   className: '',
-  icon: undefined,
+  leftIcon: undefined,
   rightIcon: undefined,
   isDisabled: false,
   isPending: false,
